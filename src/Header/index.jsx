be53a27f4f217950b8950eed3691e1759d335299
JavaBorka <borka34@gmail.com>
2023-10-16 22:10:24 +0200

    Vytvorená komponenta Header

---
+++ src/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './style.css'
+
+export const Header = ({title}) => {
+
+    return (
+        <header className="header">
+            <h1 className="header__title">{title}</h1>
+        </header>
+    )
+}
+
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { smartHomeData} from './smartHomeData'
+import { Header } from './Header/index.jsx'
 import './style.css';
 
 const App = () => (
   <>
-
+    <div className="container">
+      <Header title="Chytrý dům" />
+    </div>
   </>
 );
 
